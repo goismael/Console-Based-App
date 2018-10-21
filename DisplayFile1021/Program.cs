8b92d2107c63745da3340e6b3f4eeeda9f9545ab
Ismael <42499036+goismael@users.noreply.github.com>
2018-10-21 01:31:31 -0400

    Add files via upload

---
+++ DisplayFile1021/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DisplayFile1021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            foreach(string item in lines)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+            }
+        }
+    }
+}
